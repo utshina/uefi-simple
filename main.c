a689163e8feb428cd89d8e0c60fa2bc23a556a2d
Pete Batard <pete@akeo.ie>
2016-03-25 10:41:23 +0000

    add some colors to the printed output

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*
  * UEFI:SIMPLE - UEFI development made easy
- * Copyright © 2014-2015 Pete Batard <pete@akeo.ie> - Public Domain
+ * Copyright © 2014-2016 Pete Batard <pete@akeo.ie> - Public Domain
  * See COPYING for the full licensing terms.
  */
 #include <efi.h>
@@ -13,9 +13,18 @@ EFI_STATUS EfiMain(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 
 	InitializeLib(ImageHandle, SystemTable);
 
-	Print(L"\n*** UEFI:SIMPLE ***\n\n");
+	/*
+	 * In addition to the standard %-based flags, Print() supports the following:
+	 *   %N       Set output attribute to normal
+	 *   %H       Set output attribute to highlight
+	 *   %E       Set output attribute to error
+	 *   %B       Set output attribute to blue color
+	 *   %V       Set output attribute to green color
+	 *   %r       Human readable version of a status code
+	 */
+	Print(L"\n%H*** UEFI:SIMPLE ***%N\n\n");
 
-	Print(L"\nPress any key to exit.\n");
+	Print(L"%EPress any key to exit.%N\n");
 	SystemTable->ConIn->Reset(SystemTable->ConIn, FALSE);
 	while (SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key) == EFI_NOT_READY);
 #if defined(_DEBUG)
